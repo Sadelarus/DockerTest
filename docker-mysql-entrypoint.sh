3947a60bea13a6a1873ba0e9e1fd3c5bb3b007e0
Your Name <you@example.com>
2022-07-16 00:44:41 +0300

    Run mysql with regular user permissions

--- docker-mysql-entrypoint.sh
+++ docker-mysql-entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh -fx
-###exec /bin/bash --login
+echo USER: ${USER}
+chown -R ${USER} /var/run/mysqld/ /var/lib/mysql/
 
-chown mysql:mysql /var/lib/mysql/
+####exec sudo -u ${USER} /bin/bash --login
+
 if [ ! -f /var/lib/mysql/.db-ready ]; then
-    sudo -u mysql mysql_install_db --user=mysql --datadir=/var/lib/mysql/
-    sudo touch /var/lib/mysql/.db-ready
+    sudo -u ${USER} mysql_install_db --defaults-file=/etc/mysql/mysql.conf --user=${USER} --datadir=/var/lib/mysql/
+    sudo -u ${USER} touch /var/lib/mysql/.db-ready
 fi
 
-sudo -u mysql mariadbd
-
-
-
-
+sudo -u ${USER} \
+    /usr/libexec/mysqld \
+        --defaults-file=/etc/mysql/mysql.conf   \
+        --datadir=/var/lib/mysql/               \
+        --socket=/var/run/mysqld/mysqld.socket  \
+        --pid-file=/var/run/mysqld/mysqld.pid
--- run-mysql.sh
+++ run-mysql.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -fx
 
-mkdir -p root-mysql/var/lib/mysql
-sudo docker-compose -f ./docker-compose-mysql.yaml build
-sudo docker-compose -f ./docker-compose-mysql.yaml run mysql_test
+docker-compose -f ./docker-compose-mysql.yaml build
+docker-compose -f ./docker-compose-mysql.yaml run mysql_test
