7e21dec76cd978ede63a38812407db9cfeed2d76
Your Name <you@example.com>
2022-08-16 18:27:58 +0300

    Normal DB unforget one file

--- Create_Db.sql
+++ Create_Db.sql
@@ -1,12 +1,25 @@
 ---Create database and table
 create database if not exists newuser_db;
 Use newuser_db;
-create table if not exists employee (firstname char(20) primary key, lastname char(20), age_int int, your_number_float float, your_number_bigint bigint);
+create table if not exists shops (shop_id int primary key,
+                                  name varchar(20) not null);
+create table if not exists vendor (vendor_id int primary key,
+                                   vendor_part_name varchar(20) not null,
+                                   vendor_part_id_full varchar(20) not null);
+create table if not exists parts (part_id int primary key,
+                                  vendor_part_id int(20) not null,
+                                  constraint fk_vendor_type FOREIGN KEY (vendor_part_id) REFERENCES vendor (vendor_id));
+create table if not exists prices (part_id_p int not null,
+                                   constraint fk_part_type FOREIGN KEY (part_id_p) REFERENCES parts (part_id),
+                                   shop_id_p int not null,
+                                   constraint fk_shop_type FOREIGN KEY (shop_id_p) REFERENCES shops (shop_id),
+                                   price int not null,
+                                   amount int not null);
 
 ---Create user account to work with database
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '123';
-GRANT ALL PRIVILEGES ON *.* TO 'root';
+---GRANT ALL PRIVILEGES ON *.* TO 'root';
 CREATE USER IF NOT EXISTS 'newuser' IDENTIFIED BY '123';
 ALTER USER 'newuser' IDENTIFIED BY '123';
 GRANT All Privileges ON newuser_db.* TO 'newuser';
